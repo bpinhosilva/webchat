118a7548c1cefb0058a64df011d394e1db2af5df
Bruno Sampaio Pinho <bpinhosilva@gmail.com>
2015-05-19 16:16:11 -0300

    Basic core for webchat done
    Develop the basic of webchat to start from.

--- server.js
+++ server.js
@@ -3,3 +3,26 @@
   Bruno Pinho
   May 19th, 2015
 */
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(80, function(){
+  console.log('listening on *:80');
+});
